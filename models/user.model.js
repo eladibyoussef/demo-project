6f8862389a20ecea4d522cdf5e48de71641e0d4e
eladibyoussef <eladibyoussef230@gmail.com>
2024-03-03 14:00:33 +0100

    updated the user model

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const UserSchema =new  mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:false
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isAdmin:{
+        type: Boolean,
+        required:true
+    }
+   //other feilds will be added later after the intial set of the main routes .
+},
+
+{timesatamps:true});
+module.exports = mongoose.model('User',UserSchema)
