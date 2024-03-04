99f1cce02318af21cc0d61faf3ffdf477c9d3c5e
eladibyoussef <eladibyoussef230@gmail.com>
2024-03-04 20:43:02 +0100

    created brach to set the register/login features

--- app.js
+++ app.js
@@ -30,6 +30,104 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(passport.initialize());
+//test 
+
+
+app.post('/admin',async (req,res)=>{
+    const {email,password} = req.body;
+try{
+   const admin = await Admin.findOne({email:email})
+   if(!admin){
+    res.status(400).json({msg:'invalid crediantials'})
+   }else{
+    console.log(admin);
+    const passwordsMatched = await bcrypt.compare(password, admin.password);
+    if(passwordsMatched){
+        const payload = {id: admin.id , name: admin.name , isAdmin: admin.isAdmin};
+        console.log('payload :', payload);
+        console.log('secret', process.env.ADMIN_SECRET);
+         jwt.sign(payload, process.env.ADMIN_SECRET, {expiresIn:3600000 }, (err,token)=>{
+            res.cookie('token', token , {
+                maxAge:360000
+            });
+            res.status(200).json({msg:'admin logged successfully '})
+         })
+    }else{
+        res.status(500).json({msg:'invalid crediantials'})
+    }
+   }
+        
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+        console.log('fatal error ', error);
+    }
+    
+
+
+})
+
+app.get('/protected',authenticateAdmin,(req,res)=>{
+    res.status(201).json({msg:'you are in the admin area'})
+} )
+
+app.post('/user',async (req,res)=>{
+    const {name , email , password}=req.body;
+     try {
+        const salt = await bcrypt.genSalt(10);
+        const hashed = await bcrypt.hash(password,salt);
+
+
+        const newUser = new User({
+            name : name ,
+            email:email,
+            password:hashed
+            
+        })
+        await newUser.save();
+        console.log('user created ');
+        res.status(201).json({msg:'success', user : newUser})
+        
+     } catch (error) {
+        res.status(401).json({msg:error.msg});
+     }
+})
+
+app.post('/userlogin',async (req,res)=>{
+    const {email,password}=req.body;
+   try {
+    const user= await User.findOne({email:email});
+    if(user){
+        const passMatch= await bcrypt.compare(password,user.password);
+        if(passMatch){
+            const payload = {id: user.id , name: user.name}
+            // console.log(payload);
+            // console.log('user secret',process.env.USER_SECRET);
+            jwt.sign(payload,process.env.USER_SECRET,{expiresIn:3600000},(err,token)=>{
+                // console.log(token);
+                res.cookie('token',token,{
+                    maxAge:3600000
+                })
+                res.status(200).json({msg:'user logged successfully '})
+
+            })
+
+        }else{
+            res.status(500).json({msg:'invalid crediantials'});
+
+        }
+    }
+   } catch (error) {
+    res.status(500).json({msg: error.message})
+    console.log('fatal error ', error);
+    
+
+   }
+} )
+
+app.get('/userprotected',authenticateUser, (req,res)=>{
+    res.json({message:'you are in the user protected area '})
+} )
+
 
 
 app.listen(PORT, ()=>{
