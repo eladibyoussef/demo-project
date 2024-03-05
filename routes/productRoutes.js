6a1a21de6f7d3aaab898dd7ec2b45481d3f49f5e
eladibyoussef <eladibyoussef230@gmail.com>
2024-03-05 23:36:50 +0100

    implemented the search functionality with model,brand and gender

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productsControllers')
 
 
 
+router.get('/search',productController.searchForProduts)
+
+
+module.exports = router;
