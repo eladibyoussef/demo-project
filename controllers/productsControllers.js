3555b59dfcf66526c09c83fd3b6209a4b836e61d
Maroua <marouabenali040@gmail.com>
2024-03-06 10:14:58 +0100

    create product

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,17 +1,27 @@
 const Product = require('../models/product.model');
 
 const createProduct = async (req, res) => {
-    const product = new productModel({
-        model: req.body.model,
-        brand: req.body.brand,
-        description: req.body.description,
-        gender: req.body.gender,
-        price: req.body.price,
-    });
-
     try {
-        const newProduct = await Product.save();
-        res.status(201).json(newProduct);
+        const existingProduct = await Product.findOne({ model: req.body.model });
+
+        if (existingProduct) {
+            return res.status(409).json({ message: "Product already exists" });
+        }
+        const product = new Product({
+            model: req.body.model,
+            brand: req.body.brand,
+            description: req.body.description,
+            gender: req.body.gender,
+            price: req.body.price
+        });
+
+        const newProduct = await product.save();
+        
+        if (newProduct) {
+            res.status(201).json({ message: "Product created" });
+        } else {
+            res.status(500).json({ message: "Failed to create product" });
+        }
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
--- models/product.model.js
+++ models/product.model.js
@@ -21,6 +21,10 @@ const ProductSchema =new  mongoose.Schema({
         type:Number,
         required:true
     },
+    quantity :{
+        type:Number,
+        required:true
+    },
     createdAt :{
         type:Date,
         default: Date.now()
