d4c48a5b46422beef47d935aebaa86a73cc46c9c
Maroua <marouabenali040@gmail.com>
2024-03-28 11:45:14 +0000

    saller

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,10 +1,9 @@
 const User = require('../models/user.model');
-const Admin=require('../models/admin.model')
+const Admin = require('../models/admin.model')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 //admin login
-
 const adminLogin = async (req,res)=>{
     const {email,password} = req.body;
 try{
@@ -41,12 +40,7 @@ try{
 // Controller function to get all users
 const getAllUsers = async (req, res) => {
     try {
-
-        // Use the User model to find all users in the database
         const users = await User.find();
-
-
-        // Send the array of users as the response
         res.send(users);
     } catch (error) {
         res.status(500).send('Internal Server Error');
@@ -57,17 +51,10 @@ const getAllUsers = async (req, res) => {
 // Controller function to update a user by ID
 const UpdatUserById = async (req, res) => {
     try {
-
-        // Use the User model to find and update a user by their ID
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        
-
-        // Check if the user was not found
-        if (!user) {
+            if (!user) {
             return res.status(404).send('User not found');
         }
-
-        // Send the updated user as the response
         res.send(user);
     } catch (error) {
         res.status(500).send('Internal Server Error');
@@ -79,11 +66,7 @@ const UpdatUserById = async (req, res) => {
 const DelelUserById =  async (req, res) => {
     try {
 
-        // Use the User model to find and remove a user by their ID
         const user = await User.findByIdAndRemove(req.params.id);
-        
-
-        // Check if the user was not found
         if (!user) {
             return res.status(404).send('User not found');
         }
